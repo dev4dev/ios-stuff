65c56359117f01af4f4f760539ad69a60eba8d16
Alex Antonyuk <alex@antonyuk.me>
2012-12-09 15:39:04 +0200

    added macroses OVERRIDE - to use in methods of abstract class which have to be overridden in inherited classed

---
+++ macroses/Macroses.h
@@ -0,0 +1,14 @@
+//
+//  Macroses.h
+//  components
+//
+//  Created by Alex Antonyuk on 12/9/12.
+//  Copyright (c) 2012 Alex Antonyuk. All rights reserved.
+//
+
+#ifndef components_Macroses_h
+#define components_Macroses_h
+
+#define OVERRIDE	NSLog(@"[OVERRIDE] function %s:line %d in inherited %@ class.", __PRETTY_FUNCTION__, __LINE__, [self class])
+
+#endif
