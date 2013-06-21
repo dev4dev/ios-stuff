99a5b241df2f54caed48ba72d09960493d049c54
Alex Antonyuk <alex@antonyuk.me>
2013-06-21 10:52:28 +0300

    added Types.h

---
+++ stuff/macroses/Types.h
@@ -0,0 +1,14 @@
+//
+//  Types.h
+//  ios-stuff
+//
+//  Created by Alex Antonyuk on 6/21/13.
+//  Copyright (c) 2013 Alex Antonyuk. All rights reserved.
+//
+
+#ifndef ios_stuff_Types_h
+#define ios_stuff_Types_h
+
+typedef void(^VoidBlock)(void);
+
+#endif
